2063cd9c25c511d499531f8796b37ebdd12d5e20
Edwin Pajuelo <89942271+LibraEdwin@users.noreply.github.com>
2022-10-14 14:40:30 -0500

    template

---
+++ src/app.module.ts
@@ -0,0 +1,47 @@
+import { Module } from '@nestjs/common';
+import { AppController } from './app.controller';
+import { AppService } from './app.service';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { WinstonModule } from 'nest-winston';
+import * as winston from 'winston';
+import * as path from 'path';
+import { configService } from './shared/config/config.service';
+
+@Module({
+  imports: [
+    TypeOrmModule.forRoot(
+      configService.getTypeOrmConfig(),),
+    WinstonModule.forRoot({
+      level: 'debug',
+      format: winston.format.combine(
+        winston.format.timestamp({
+          format: 'YYYY-MM-DD HH:mm:ss',
+        }),
+        winston.format.errors({ stack: true }),
+        winston.format.splat(),
+        winston.format.json(),
+      ),
+      transports: [
+        new winston.transports.File({
+          dirname: path.join(__dirname, './../log/debug/'),
+          filename: 'debug.log',
+          level: 'debug',
+        }),
+        new winston.transports.File({
+          dirname: path.join(__dirname, './../log/error/'),
+          filename: 'error.log',
+          level: 'error',
+        }),
+        new winston.transports.File({
+          dirname: path.join(__dirname, './../log/info/'),
+          filename: 'info.log',
+          level: 'info',
+        }),
+        new winston.transports.Console({ level: 'debug' }),
+      ],
+    }),
+  ],
+  controllers: [AppController],
+  providers: [AppService],
+})
+export class AppModule { }
---
+++ src/shared/pagination.dto.ts
@@ -0,0 +1,15 @@
+import { IsNumber, IsPositive, IsOptional } from 'class-validator';
+export class PaginationDto {
+    @IsNumber()
+    @IsPositive()
+    @IsOptional()
+    inicio: number;
+
+    @IsNumber()
+    @IsPositive()
+    @IsOptional()
+    pageSize: number;
+
+    @IsOptional()
+    text?: string | null
+}   
